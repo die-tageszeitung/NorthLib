d7d4f00b0768dbc563f927e5438e29de11bc8360
Ringo <M22>
2025-05-30 12:29:40 +0200

    added helper to add/remove local notifications

--- src/Foundation/LocalNotifications.swift
+++ src/Foundation/LocalNotifications.swift
@@ -13,6 +13,7 @@ open class LocalNotifications: DoesLog {
   
   ///Helper to trigger local Notification if App is in Background
   ///use payload to add custom data
+  @discardableResult
   public static func notify(title:String? = nil,
                             subtitle:String? = nil,
                             message:String,
@@ -21,10 +22,9 @@ open class LocalNotifications: DoesLog {
                             attachmentURL: URL? = nil,
                             categoryIdentifier: String? = nil,
                             notificationIdentifier: String? = nil,
-                            payload: [AnyHashable : Any]? = nil,
-                            delay: TimeInterval = 5.0
-
-  ){
+                            payload: [AnyHashable: Any]? = nil,
+                            delay: TimeInterval = 5.0) -> String {
+    
     let fireDate = Date().addingTimeInterval(delay)
     let identifier = notificationIdentifier ?? "Notification-\(title ?? subtitle ?? message)-\(fireDate.ddMMyy_HHmmss)"
     
@@ -45,19 +45,27 @@ open class LocalNotifications: DoesLog {
     if let categoryIdentifier = categoryIdentifier { content.categoryIdentifier = categoryIdentifier }
     if let badge = badge { content.badge = NSNumber(value: badge) }
     
-
-    let dateComponents = Calendar.current.dateComponents(Set(arrayLiteral: Calendar.Component.year, Calendar.Component.month, Calendar.Component.day, Calendar.Component.hour, Calendar.Component.minute, Calendar.Component.second), from: fireDate)
+    let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: fireDate)
     let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-    let request = UNNotificationRequest(identifier: "\(identifier)-ni",
-                                        content: content,
-                                        trigger: trigger)
-    UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+    let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+    
+    UNUserNotificationCenter.current().add(request) { error in
       if let error = error {
-        Log.log("notification with error: \(error)")
+        Log.log("Notification setup failed with error: \(error)")
       } else {
-        Log.log("notification setup succeed")
+        Log.log("Notification setup succeed with ID: \(identifier)")
       }
-    })
-    Log.log("notification added for \(fireDate.ddMMyy_HHmmss)")
+    }
+    
+    Log.log("Notification added for \(fireDate.ddMMyy_HHmmss) with ID: \(identifier)")
+    return identifier
+  }
+  
+  /// Function to remove a specific notification by its identifier
+  public static func removeNotification(identifier: String?) {
+    guard let identifier = identifier else { return }
+    UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [identifier])
+    UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: [identifier])
+    Log.log("Notification with ID \(identifier) removed")
   }
 }
