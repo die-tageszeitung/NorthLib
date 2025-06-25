5dfb513ece226378fb64fbf7ec34495e0f2f18cf
Ringo <M22>
2025-06-25 22:23:46 +0200

    still crashing...reverted

--- src/Foundation/Network/NetAvailability.swift
+++ src/Foundation/Network/NetAvailability.swift
@@ -225,11 +225,6 @@ public class ExtendedNetAvailability: DoesLog {
       return
     }
     
-    if force && OperationQueue.current?.underlyingQueue == nil {
-      log("prevent crash due netAvailability force recheck & creation")
-      return
-    }
-    
     if force || createNetAvailability()?.reachabilityFlags != netAvailability.lastFlags {
       self.netAvailability = createNetAvailability()
       _onChangeClosure?(self.netAvailability?.isAvailable ?? false)
